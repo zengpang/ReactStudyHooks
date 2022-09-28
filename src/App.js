fbf6f903f72c0e0ace2e987921558d7d6b7267d9
zengpang <2516629927@qq.com>
2022-09-28 18:17:40 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState ,useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//useState
+// function App(){
+//   const[count,setCount]=useState(0);
+//   return(
+//     <div className='App'>
+//       <p>You clicked{count} times</p>
+//       <button onClick={()=>setCount(count+1)}>点击我</button>
+//     </div>
+//   );
+// }
+
+//多个useState
+// function App(){
+//   const[count,setCount]=useState(0);
+//   const[age,setAge]=useState(20);
+//   return(
+//     <div className='App'>
+//       <p>你点击了{count}次数</p>
+//       <button onClick={()=>setCount(count+1)}>点击我</button>
+//       <p>你的年龄{age} times</p>
+//       <button onClick={()=>setAge(age+1)}>点击增加年龄</button>
+//     </div>
+//   );
+// }
+
+//useEffect
+// function Counter(){
+//   const [count,setCount]=useState(0);
+//   const [age,setAge]=useState(0);
+//   //相当于componentDidMount 和 componentDidUpdate
+//   useEffect(()=>{
+//     console.log(`You clicked ${count}`);
+//   });//监控所有state,只要有任意state变化都会触发
+//   return(
+//     <div className='App'>
+//      <p>You clicked{count} times</p>
+//      <button onClick={()=>{setCount(count+1)}}>Add count</button>
+//      <p>You are {age} years old!</p>
+//      <button onClick={()=>{setAge(age+1)}}>Add age</button>
+//     </div>
+//   );
+// }
+
+//useEffect针对性监控
+function Counter(){
+  const [count,setCount]=useState(0);
+  const [age,setAge]=useState(0);
+  //相当于componentDidMount 和 componentDidUpdate
+  useEffect(()=>{
+    console.log(`挂载和状态更新时执行`);
+    return()=>{
+      console.log(`状态更新和卸载组件时执行`);
+    }
+  },[count]);//只监控count状态
+  return(
+    <div className='App'>
+     <p>You clicked{count} times</p>
+     <button onClick={()=>{setCount(count+1)}}>Add count</button>
+     <p>You are {age} years old!</p>
+     <button onClick={()=>{setAge(age+1)}}>Add age</button>
     </div>
   );
 }
 
-export default App;
+export default Counter;
+
+
